9234d448f0e93e25069e051be88d8d94bb733915
maguiag <36174618+maguiag@users.noreply.github.com>
2018-08-29 22:17:21 -0300

    Add files via upload

--- Clase_03/main.c
+++ Clase_03/main.c
@@ -25,21 +25,25 @@ int getEdad(int*pEdad)
 
     for(i=0; i<reintentos; i++) // reintentos=3; reintentos>0; reintentos--
     {
-    printf("Ingrese la edad: ");
-    if(scanf("%d", &auxEdad)==1);
-    {
-        if(auxEdad>=0 && auxEdad<199)
+        printf("Ingrese la edad: ");
+        if(scanf("%d", &auxEdad)==1);
+        {
+            if(auxEdad>=0 && auxEdad<199)
             { //
              *pEdad =auxEdad;
              retorno = 0;
              break;
             }
-    }
+            else
+            {
+
+            }
+        }
         else
-         {
-          printf("La edad esta fuera de rango");
-          __fpurge(stdin); // vaciamos standar imput
-         }
+        {
+            printf("La edad esta fuera de rango");
+            __fpurge(stdin); // vaciamos standar imput
+        }
     }
  return retorno;
 }
