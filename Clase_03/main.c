965fd26ad3e3da3768a0c5c1e07a66348a9a2136
maguiag <36174618+maguiag@users.noreply.github.com>
2018-08-29 22:06:18 -0300

    Add files via upload

---
+++ Clase_03/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int getEdad(int *pEdad);
+
+
+int main()
+{
+ int edad;
+
+ if(getEdad(&edad)==0)
+ {
+  printf("La edad es: %d",edad);
+ }
+
+    return 0;
+}
+
+int getEdad(int*pEdad)
+{
+    int retorno = -1;
+    int auxEdad;
+    int i;
+    int reintentos =3;
+
+    for(i=0; i<reintentos; i++) // reintentos=3; reintentos>0; reintentos--
+    {
+    printf("Ingrese la edad: ");
+    if(scanf("%d", &auxEdad)==1);
+    {
+        if(auxEdad>=0 && auxEdad<199)
+            { //
+             *pEdad =auxEdad;
+             retorno = 0;
+             break;
+            }
+    }
+    else
+    {
+    printf("La edad esta fuera de rango");
+    }
+    }
+ return retorno;
+}
