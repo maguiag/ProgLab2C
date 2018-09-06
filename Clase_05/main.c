fd61ee6e1157582a9331b8db9a7b9b00f9f1bba7
maguiag <36174618+maguiag@users.noreply.github.com>
2018-09-05 22:14:20 -0300

    Add files via upload

--- Clase_05/main.c
+++ Clase_05/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "utn.h"
 #define CANTIDAD_EMPLEADOS 5
+#define DATO_INVALIDO -1
 
 int mostrarArray(int* pArray,int limite)
 
@@ -18,7 +19,7 @@ int main()
         //o &edades[i]=>(edades +1) (es algebra de punteros)
         // if(!utn_()) niega todo lo que pasa en utn_get pero lo reemplazo x ==0 al final. -1 porque es si sale mal
         {
-            edades[i]=-1; //pongo -1 en la posicion i; -1 es que la fc no levanto edad
+            edades[i]=DATO_INVALIDO; //pongo -1 en la posicion i; -1 es que la fc no levanto edad
         }
     }
 
@@ -44,30 +45,61 @@ int mostrarArray(int* pArray,int limite) //o int pArray[]
     return 0;
 }
 
-int calcularMaximo(int* pArray, int limite, )
-{
-    int i;
-    int maximo;
-    int auxM;
-
-    for(i=0; i<limite; i++)
-    {
-
-    }
-
-
-    return 0;
-}
-
-
-int initArray(int* pArray,int limite)
+int initArray(int* pArray,int limite, int valor)
 {
     int i;
     int retorno=-1
-    if (pArray != NULL && limite>0)
+    if (pArray != NULL && limite>0)//null es 0?
       {
+        for(i=0; i=limite;i++)
+        {
+            pArray[i]=valor;
+            //*(pArray+i)=valor;
+        }
         retorno = 0;
       }
 
     return retorno;
+}
+
+
+int calcularMaximo(int* pArray, int limite )
+{
+    int i;
+    int retorno=-1;
+    int maximo;
+    int flagPrimerMaximo=0; //false
+
+    if(pArray!=NULL %% limite>0)
+    {//descarta -1 para conteo y par acumulacion de edades
+        //-1 era error, lo tengo que descartar de la busqueda. por eso uso un flag
+        for(i=0; i<limite;i++)
+        {
+            if(pArray[i] !=DATO_INVALIDO)
+            {
+                if(flagPrimerMaximo==0) //false
+                {
+                    maximo=pArray[i];
+                    flagPrimerMaximo=1; //true
+                }
+                else if(pArray[i]>maximo)
+                    {
+                        maximo=pArray[i];
+
+                    }
+            }
+
+        }
+        if(flagPrimerMaximo==0)
+        {
+        retorno=-2;
+        }
+        else
+        {
+            retorno=0;
+        }
+    }
+
+
+    return 0;
 }
