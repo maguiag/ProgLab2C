ea7ec964c1e55b82ee0b66265fbbdcd19f0d2c54
maguiag <36174618+maguiag@users.noreply.github.com>
2018-09-19 20:19:19 -0300

    Add files via upload

--- Clase_09/main.c
+++ Clase_09/main.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <stdio_ext.h>
 #include <string.h>
-#include "stdio_ext.h"
-#include "utn.h"
+#include "producto.h"
+#define CANTIDAD 200
 
 int main()
 {
+    Producto productos[CANTIDAD];//P es la estruct; p es el array
+    inicializarArray(productos, CANTIDAD); //ARRAY, LIMITE
+    imprimirArray(productos,CANTIDAD);
+    cargarProducto(productos,5);
 
     return 0;
 }
