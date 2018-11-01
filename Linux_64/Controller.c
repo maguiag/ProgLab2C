5c5d3de908a9961e22045d90840e33ad289d010a
maguiag <36174618+maguiag@users.noreply.github.com>
2018-10-31 21:02:39 -0300

    Add files via upload

---
+++ Linux_64/Controller.c
@@ -0,0 +1,166 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+#include "Employee.h"
+
+
+/** \brief Carga los datos de los empleados desde el archivo data.csv (modo texto).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_loadFromText(char* path , LinkedList* pArrayListEmployee)
+{
+
+    int retorno=0;
+    FILE* pArchivoEmployee;
+    pArchivoEmployee=fopen("data.csv","r");
+    if(parser_EmployeeFromText(pArchivoEmployee,pArrayListEmployee))
+    {
+        retorno=1;
+    }
+    else
+    {
+        printf("Error de Archivo");
+    }
+    fclose(pArchivoEmployee);
+
+    return retorno;
+
+}
+
+/** \brief Carga los datos de los empleados desde el archivo data.csv (modo binario).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_loadFromBinary(char* path , LinkedList* pArrayListEmployee)
+{
+    int retorno=-1;
+    FILE * pArchivo;
+    pArchivo=fopen(path, "r");
+    retorno=parser_EmployeeFromBinary(pArchivo,pArrayListEmployee);
+    fclose(pArchivo);
+    return retorno;
+}
+
+/** \brief Alta de empleados
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_addEmployee(LinkedList* pArrayListEmployee)
+{
+    return 1;
+}
+
+/** \brief Modificar datos de empleado
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_editEmployee(LinkedList* pArrayListEmployee)
+{
+    return 1;
+}
+
+/** \brief Baja de empleado
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_removeEmployee(LinkedList* pArrayListEmployee)
+{
+    return 1;
+}
+
+/** \brief Listar empleados
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_ListEmployee(LinkedList* pArrayListEmployee)
+{
+    int retorno=-1;
+    int i;
+    void* pEmpleado;
+    char nombre[50];
+    if(pArrayListEmployee != NULL)
+    {
+        retorno = 0;
+        for(i=0;i < ll_len(pArrayListEmployee);i++)
+        {
+            pEmpleado = ll_get(pArrayListEmployee,i);
+            employee_getNombre(pEmpleado,nombre);
+            printf("\nNombre: %s", nombre);
+        }
+    }
+    return retorno;
+
+
+
+    return 1;
+}
+
+/** \brief Ordenar empleados
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_sortEmployee(LinkedList* pArrayListEmployee)
+{
+    return 1;
+}
+
+/** \brief Guarda los datos de los empleados en el archivo data.csv (modo texto).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_saveAsText(char* path , LinkedList* pArrayListEmployee)
+{
+    return 1;
+}
+
+/** \brief Guarda los datos de los empleados en el archivo data.csv (modo binario).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_saveAsBinary(char* path , LinkedList* pArrayListEmployee)
+{
+    FILE *pArchivo = fopen(path,"wb"); //wb write en binary
+    Employee* pEmpleado;
+    int i;
+    int lenArray=ll_len(pArrayListEmployee);
+    if(pArchivo != NULL)
+    {
+        for(i=0;i<lenArray;i++)
+        {
+        pEmpleado=ll_get(pArrayListEmployee,i);
+        fwrite(pEmpleado,sizeof(Employee),1,pArchivo);
+        //*a lo q uiero volcar(empleado), cuanto pesa,cuantos empleados!nunca en len de a 1,donde lo escribo
+        }
+
+    }
+    return 1;
+}
+
---
+++ Linux_64/parser.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+#include "Employee.h"
+
+/** \brief Parsea los datos los datos de los empleados desde el archivo data.csv (modo texto).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int parser_EmployeeFromText(FILE* pFile , LinkedList* pArrayListEmployee)
+{
+
+    char bufferInt[1024];
+    char bufferNombre[1024];
+    char bufferHorasTrabajadas[1024];
+    char bufferSueldo[1024];
+    int retorno = -1;
+    int flagOnce=1;
+    Employee * pEmpleado;
+
+
+    if(pFile != NULL)
+    {
+        while(!feof(pFile))
+        {
+            if(flagOnce)
+            {
+                flagOnce = 0;
+                fscanf(pFile,"%[^,],%[^,],%[^,],%[^\n]\n",
+                                        bufferInt,
+                                        bufferNombre,
+                                        bufferHorasTrabajadas,
+                                        bufferSueldo);
+            }
+            fscanf(pFile,"%[^,],%[^,],%[^,],%[^\n]\n",
+                                        bufferInt,
+                                        bufferNombre,
+                                        bufferHorasTrabajadas,
+                                        bufferSueldo);
+            //printf("\n%s",bufferNombre);
+
+            pEmpleado = employee_newConParametros(
+                                        bufferInt,
+                                        bufferNombre,
+                                        bufferHorasTrabajadas,
+                                        bufferSueldo);
+
+            if(pEmpleado != NULL)
+            {
+                retorno = 0;
+                ll_add(pArrayListEmployee,pEmpleado);
+            }
+
+        }
+    }
+    return retorno;
+}
+
+/** \brief Parsea los datos los datos de los empleados desde el archivo data.csv (modo binario).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int parser_EmployeeFromBinary(FILE* pFile , LinkedList* pArrayListEmployee)
+{
+    Employee *pEmpleado; // ver si es razonable sumar funcion de validacion
+//fc de consistencia !!!
+    do
+    {
+        pEmpleado=employee_new(); //distinto parametro xq no lo tengo
+        fread(pEmpleado,sizeof(Employee),1,pFile);// pide donde dejo, no origen
+        ll_add(pArrayListEmployee,pEmpleado)
+    }
+    while(!feof(pFile));
+    return 1;
+}
+
+/*
+Employee * pEmpleado;
+    int size=0;
+    char bufferInt[1024];
+    char bufferNombre[1024];
+    char bufferHorasTrabajadas[1024];
+    char bufferSueldo[1024];
+    int flagOnce=1;
+    int rerono =-1
+
+
+  if(pFile!=NULL)
+    {
+        while(!feof(pFile))
+        {
+            fscanf(pArchivoCliente,"%s\n",bufferId);
+            flagOnce = 0;
+        }
+        fscanf(pArchivoCliente,"%[^,],%[^,],%[^,],%[^\n]\n",
+                                        bufferId,
+                                        bufferNombre,
+                                        bufferApellido,
+                                        bufferIsEmpty);
+
+        auxiliarPunteroCliente = cliente_newConParametros(
+                                        bufferId,
+                                        bufferNombre,
+                                        bufferApellido);
+
+        if(auxiliarPunteroCliente != NULL)
+        {
+            arrayPunteroCliente[size] = auxiliarPunteroCliente;
+            size++;
+        }
+    }
+    fclose(pArchivoCliente);
+
+
+    mostrar(arrayPunteroCliente,size);
+    guardar(arrayPunteroCliente,size);
+    return 0;
+}
+
