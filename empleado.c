b1abc263512c6dfb92453bf29a721bb00fd7879b
maguiag <36174618+maguiag@users.noreply.github.com>
2018-11-15 19:14:24 -0300

    Add files via upload

---
+++ empleado.c
@@ -0,0 +1,131 @@
+
+#include "empleado.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+Empleado* empleado_new()
+{
+    Empleado* this;
+    this=malloc(sizeof(Empleado));
+    return this;
+}
+
+void empleado_delete(Empleado* this)
+{
+    free(this);
+}
+
+Empleado* empleado_newConParametros(int id,char* nombre,int horasTrabajadas,int sueldo)
+{
+    Empleado* this;
+    this=empleado_new();
+
+    if(
+    !empleado_setId(this,id)&&
+    !empleado_setNombre(this,nombre)&&
+    !empleado_setHorasTrabajadas(this,horasTrabajadas)&&
+    !empleado_setSueldo(this,sueldo))
+        return this;
+
+    empleado_delete(this);
+    return NULL;
+}
+
+int empleado_setId(Empleado* this,int id)
+{
+    int retorno=-1;
+    static int proximoId=-1;
+
+    if(this!=NULL && id==-1)
+    {
+        proximoId++;
+        this->id=proximoId;
+        retorno=0;
+    }
+    else if(id>proximoId)
+    {
+        proximoId=id;
+        this->id=proximoId;
+        retorno=0;
+    }
+    return retorno;
+}
+
+int empleado_getId(Empleado* this,int* id)
+{
+    int retorno=-1;
+    if(this!=NULL)
+    {
+        *id=this->id;
+        retorno=0;
+    }
+    return retorno;
+}
+
+int empleado_setNombre(Empleado* this,char* nombre)
+{
+    int retorno=-1;
+    if(this!=NULL && nombre!=NULL)
+    {
+        strcpy(this->nombre,nombre);
+        retorno=0;
+    }
+    return retorno;
+}
+
+int empleado_getNombre(Empleado* this,char* nombre)
+{
+    int retorno=-1;
+    if(this!=NULL && nombre!=NULL)
+    {
+        strcpy(nombre,this->nombre);
+        retorno=0;
+    }
+    return retorno;
+}
+
+int empleado_setHorasTrabajadas(Empleado* this,int horasTrabajadas)
+{
+    int retorno=-1;
+    if(this!=NULL)
+    {
+        this->horasTrabajadas=horasTrabajadas;
+        retorno=0;
+    }
+    return retorno;
+}
+
+int empleado_getHorasTrabajadas(Empleado* this,int* horasTrabajadas)
+{
+    int retorno=-1;
+    if(this!=NULL)
+    {
+        *horasTrabajadas=this->horasTrabajadas;
+        retorno=0;
+    }
+    return retorno;
+}
+
+int empleado_setSueldo(Empleado* this,int sueldo)
+{
+    int retorno=-1;
+    if(this!=NULL)
+    {
+        this->sueldo=sueldo;
+        retorno=0;
+    }
+    return retorno;
+}
+
+int empleado_getSueldo(Empleado* this,int* sueldo)
+{
+    int retorno=-1;
+    if(this!=NULL)
+    {
+        *sueldo=this->sueldo;
+        retorno=0;
+    }
+    return retorno;
+}
+
---
+++ empleado.h
@@ -0,0 +1,29 @@
+typedef struct
+{
+    int id;
+    char nombre[128];
+    int horasTrabajadas;
+    int sueldo;
+}Empleado;
+
+
+
+
+
+Empleado* empleado_new();
+void empleado_delete();
+Empleado* empleado_newConParametros(int id,char* nombre,int horasTrabajadas,int sueldo);
+
+int empleado_setId(Empleado* this,int id);
+int empleado_getId(Empleado* this,int* id);
+
+int empleado_setNombre(Empleado* this,char* nombre);
+int empleado_getNombre(Empleado* this,char* nombre);
+
+int empleado_setHorasTrabajadas(Empleado* this,int horasTrabajadas);
+int empleado_getHorasTrabajadas(Empleado* this,int* horasTrabajadas);
+
+int empleado_setSueldo(Empleado* this,int sueldo);
+int empleado_getSueldo(Empleado* this,int* sueldo);
+
+#endif // EMPLEADO_H_INCLUDED
